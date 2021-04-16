886cf70852f0a877f17f5b2bb753b1e2518840b5
Sami Sezgin <sami_sezgin@hotmail.com>
2021-04-17 01:07:45 +0300

    back to business

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main() {
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
     std::cout << extensionCount << " extensions supported\n";
-std::cout<<"bokbokbok";
+
     glm::mat4 matrix;
     glm::vec4 vec;
     auto test = matrix * vec;
