f52e3d44249c6ee5535963082911608f4319d402
Sami Sezgin <sami_sezgin@hotmail.com>
2021-04-17 03:34:03 +0300

    setupDebugMessenger added

--- main.cpp
+++ main.cpp
@@ -19,6 +19,18 @@ const bool enableValidationLayers = false;
 const bool enableValidationLayers = true;
 #endif
 
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo, const VkAllocationCallbacks *pAllocator, VkDebugUtilsMessengerEXT *pDebugMessenger)
+{
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != nullptr)
+    {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    }
+    else
+    {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
 class HelloTriangleApplication
 {
 public:
@@ -26,6 +38,7 @@ public:
     {
         initWindow();
         initVulkan();
+        setupDebugMessenger();
         mainLoop();
         cleanup();
     }
@@ -33,6 +46,7 @@ public:
 private:
     GLFWwindow *window;
     VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
     void initWindow()
     {
         glfwInit();
@@ -46,6 +60,31 @@ private:
     void initVulkan()
     {
         createInstance();
+        setupDebugMessenger();
+    }
+
+    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo)
+    {
+        createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback = debugCallback;
+    }
+
+    void setupDebugMessenger()
+    {
+        if (!enableValidationLayers)
+            return;
+
+        VkDebugUtilsMessengerCreateInfoEXT createInfo;
+        populateDebugMessengerCreateInfo(createInfo);
+
+        if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS)
+        {
+            std::cout << "failed to set up debug messenger!" << std::endl;
+            return;
+        }
     }
 
     void mainLoop()
@@ -108,6 +147,16 @@ private:
         return extensions;
     }
 
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+        void *pUserData)
+    {
+        std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+        return VK_FALSE;
+    }
+
     void createInstance()
     {
         if (enableValidationLayers && !checkValidationLayerSupport())
@@ -137,13 +186,11 @@ private:
         {
             createInfo.enabledLayerCount = 0;
         }
-        
 
-        auto extensions=getRequiredExtensions();
-        createInfo.enabledExtensionCount=static_cast<uint32_t>(extensions.size());
-        createInfo.ppEnabledExtensionNames=extensions.data();
+        auto extensions = getRequiredExtensions();
+        createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+        createInfo.ppEnabledExtensionNames = extensions.data();
 
-        
         VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
 
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
@@ -151,23 +198,6 @@ private:
             std::cout << "failed to create instance" << std::endl;
             exit(1);
         }
-
-        uint32_t extensionCount = 0;
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-        std::vector<VkExtensionProperties> extensions(extensionCount);
-
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-
-        std::cout << "available extensions: \n";
-
-        for (const auto &extension : extensions)
-        {
-            std::cout << "\t" << extension.extensionName << "\n";
-        }
-        
-
-        
     }
 };
 
